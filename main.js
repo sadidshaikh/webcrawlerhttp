9afb33faf04cdceacb519e65c04e1f75f57a2492
sadidshaikh <sadid.22010049@viit.ac.in>
2023-02-07 20:45:56 +0530

    printReport finished

--- main.js
+++ main.js
@@ -2,25 +2,21 @@ const { crawlPage } = require("./crawl.js");
 const { printReport, sortPages } = require("./report.js");
 
 async function main() {
-  //   if (process.argv.length < 3) {
-  //     console.log("no website provided");
-  //     process.exit(1);
-  //   }
+  if (process.argv.length < 3) {
+    console.log("no website provided");
+    process.exit(1);
+  }
 
-  //   if (process.argv.length > 3) {
-  //     console.log("too many command line args");
-  //     process.exit(1);
-  //   }
+  if (process.argv.length > 3) {
+    console.log("too many command line args");
+    process.exit(1);
+  }
 
-  //   const baseURL = process.argv[2];
-  //   console.log(`starting crawl of ${baseURL}`);
+  const baseURL = process.argv[2];
+  console.log(`starting crawl of ${baseURL}`);
 
-  //   const pages = await crawlPage(baseURL, baseURL, {});
-  //   printReport(pages)
-
-  const res = await fetch("https://leetcode.com/contest/globalranking");
-  const data = await res.text();
-  console.log(data);
+  const pages = await crawlPage(baseURL, baseURL, {});
+  printReport(pages)
 }
 
 main();
